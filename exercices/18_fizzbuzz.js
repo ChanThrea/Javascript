401753caef8c3e7ab27442fb2a8a1bf77a5d7d79
Chanrothmony <chanthreakhy@gmail.com>
2021-12-07 16:45:48 +0100

    bonjour

---
+++ exercices/18_fizzbuzz.js
@@ -0,0 +1,19 @@
+/*
+for (var i = 1; i <= 100; i++){
+    if((i%3 === 0) && (i%5 === 0)){
+        console.log("fizzbuzz");
+    } else if(i%3 === 0){
+        console.log("Fizz");
+    } else if(i%5 === 0)
+        console.log("Buzz");
+        else{
+            console.log(i);
+        }
+}
+*/
+for (var nombre = 1; nombre <= 100; nombre++){
+    if((nombre % 3 === 0)&&(nombre % 5 === 0)) console.log("FizzBuzz");
+    else if(nombre % 3 === 0) console.log("Fizz");
+    else if(nombre % 5 === 0) console.log("Buzz");
+    else console(nombre);
+}
